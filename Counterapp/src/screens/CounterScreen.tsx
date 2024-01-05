88cd760c34e6fa1f7897ca19c2d0bd56933d6d2e
Hernan Mercado <hernandmf@gmail.com>
2024-01-05 11:28:10 -0500

    [add] stylesheet in react native

--- Counterapp/src/screens/CounterScreen.tsx
+++ Counterapp/src/screens/CounterScreen.tsx
@@ -1,25 +1,15 @@
 import {useState} from 'react';
-import {Text, View, Button, TouchableOpacity} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet} from 'react-native';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState(23);
 
   return (
-    <View
-      style={{
-        backgroundColor: 'cyan',
-        flex: 1,
-        justifyContent: 'center',
-      }}>
-      <Text
-        style={{
-          fontSize: 44,
-          textAlign: 'center',
-          top: -15,
-        }}>
-        SOFTIX USERS: {counter}
-      </Text>
-      <TouchableOpacity onPress={() => setCounter(counter + 1)}>
+    <View style={styles.container}>
+      <Text style={styles.title}>SOFTIX USERS: {counter}</Text>
+      <TouchableOpacity
+        style={styles.btn}
+        onPress={() => setCounter(counter + 1)}>
         <View>
           <Text>+1</Text>
         </View>
@@ -27,3 +17,25 @@ export const CounterScreen = () => {
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: 'cyan',
+  },
+
+  title: {
+    fontSize: 44,
+    textAlign: 'center',
+    top: -15,
+  },
+
+  btn: {
+    padding: 1,
+    backgroundColor: 'blue',
+    color: 'white',
+    fontSize: 12,
+    borderRadius: 10,
+  },
+});
