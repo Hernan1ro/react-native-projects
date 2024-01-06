2c46d8ee50c74dd70dc336678ce91879e2bfca61
Hernan Mercado <hernandmf@gmail.com>
2024-01-06 12:00:52 -0500

    [add] Height, width porcentual and screen dimensions

--- Counterapp/App.tsx
+++ Counterapp/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {CounterScreen} from './src/screens/CounterScreen';
 import {BoxObjectModelScreen} from './src/screens/BoxObjectModelScreen';
 import {SafeAreaView} from 'react-native';
+import {DimensionesScreen} from './src/screens/DimensionesScreen';
 // import {HelloWorldScreen} from './src/screens/HelloWorldScreen';
 
 export const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <BoxObjectModelScreen />
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionesScreen />
     </SafeAreaView>
   );
 };
