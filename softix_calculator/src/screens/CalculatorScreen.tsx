19850838cec4029ae0923bc1104e20adf4739587
Hernan Mercado <hernandmf@gmail.com>
2024-01-11 09:59:54 -0500

    [add] btn operators

--- softix_calculator/src/screens/CalculatorScreen.tsx
+++ softix_calculator/src/screens/CalculatorScreen.tsx
@@ -1,14 +1,24 @@
 import {View, Text} from 'react-native';
-import {useState} from 'react';
+import {useState, useRef} from 'react';
 import {styles} from '../theme/appTheme';
 import {Btn} from '../components/Btn';
+
+enum Opeators {
+  add,
+  substract,
+  multiply,
+  divide,
+}
 
 export const CalculatorScreen = () => {
   const [number, setNumber] = useState('0');
   const [prevNum, setPrevNum] = useState('0');
 
+  const lastOperation = useRef<Opeators>();
+
   const clear = () => {
     setNumber('0');
+    setPrevNum('0');
   };
 
   const buildNumber = (textNum: string) => {
@@ -64,6 +74,32 @@ export const CalculatorScreen = () => {
     }
   };
 
+  const changeNumberByPrev = () => {
+    if (number.endsWith('.')) {
+      setPrevNum(number.slice(0, -1));
+    } else {
+      setPrevNum(number);
+    }
+    setNumber('0');
+  };
+
+  const btnDivide = () => {
+    changeNumberByPrev();
+    lastOperation.current = Opeators.divide;
+  };
+  const btnMultiply = () => {
+    changeNumberByPrev();
+    lastOperation.current = Opeators.multiply;
+  };
+  const btnSubstract = () => {
+    changeNumberByPrev();
+    lastOperation.current = Opeators.substract;
+  };
+  const btnAdd = () => {
+    changeNumberByPrev();
+    lastOperation.current = Opeators.add;
+  };
+
   return (
     <View style={styles.calculatorContainer}>
       <Text style={styles.smallResult}>{prevNum == '0' ? '' : prevNum}</Text>
@@ -75,28 +111,28 @@ export const CalculatorScreen = () => {
         <Btn text="C" color="#9b9b9b" action={clear} />
         <Btn text="+/-" color="#9b9b9b" action={positiveNegative} />
         <Btn text="del" color="#9b9b9b" action={btnDelete} />
-        <Btn text="/" color="#ff9427" action={clear} />
+        <Btn text="/" color="#ff9427" action={btnDivide} />
       </View>
       {/*---------- button row ----------*/}
       <View style={styles.row}>
         <Btn text="7" action={buildNumber} />
         <Btn text="8" action={buildNumber} />
         <Btn text="9" action={buildNumber} />
-        <Btn text="X" color="#ff9427" action={clear} />
+        <Btn text="X" color="#ff9427" action={btnMultiply} />
       </View>
       {/*---------- button row ----------*/}
       <View style={styles.row}>
         <Btn text="4" action={buildNumber} />
         <Btn text="5" action={buildNumber} />
         <Btn text="6" action={buildNumber} />
-        <Btn text="-" color="#ff9427" action={clear} />
+        <Btn text="-" color="#ff9427" action={btnSubstract} />
       </View>
       {/*---------- button row ----------*/}
       <View style={styles.row}>
         <Btn text="1" action={buildNumber} />
         <Btn text="2" action={buildNumber} />
         <Btn text="3" action={buildNumber} />
-        <Btn text="+" color="#ff9427" action={clear} />
+        <Btn text="+" color="#ff9427" action={btnAdd} />
       </View>
       {/*---------- button row ----------*/}
       <View style={styles.row}>
