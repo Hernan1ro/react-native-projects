a6a3cc43fb20f3e1abb2e35a4051960ffa2caa2a
Hernan Mercado <hernandmf@gmail.com>
2024-01-12 17:26:01 -0500

    [add] fixing react dependecy error

--- navegationapp/App.tsx
+++ navegationapp/App.tsx
@@ -1,15 +1,16 @@
 import 'react-native-gesture-handler';
+
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {StackNavigator} from './src/router/StackNavigator';
+// import {StackNavigator} from './src/router/StackNavigator';
 import {AsideMenu} from './src/router/AsideMenu';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
-      <StackNavigator />
-      {/* <AsideMenu /> */}
+      <AsideMenu />
+      {/* <StackNavigator /> */}
     </NavigationContainer>
   );
 }
--- navegationapp/src/router/AsideMenu.tsx
+++ navegationapp/src/router/AsideMenu.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {StackNavigator} from './StackNavigator';
 import {SettingsScreen} from '../screens/SettingsScreen';
+import {PersonScreen} from '../screens/PersonScreen';
+import {Page1Screen} from '../screens/Page1Screen';
+import {Page2Screen} from '../screens/Page2Screen';
 
 const Drawer = createDrawerNavigator();
 
 export const AsideMenu = () => {
   return (
     <Drawer.Navigator>
+      {/* <Drawer.Screen name="PersonScreen" component={PersonScreen} /> */}
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
+      <Drawer.Screen name="Page2Screen" component={Page2Screen} />
       <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
     </Drawer.Navigator>
   );
