80e0bf65e9cabaf08956a00cbf4f8dca8f8c0174
Hernan Mercado <hernandmf@gmail.com>
2024-01-12 12:57:04 -0500

    [add] adding drawer configuration

--- navegationapp/App.tsx
+++ navegationapp/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './src/router/StackNavigator';
+import {AsideMenu} from './src/router/AsideMenu';
 
 function App(): React.JSX.Element {
   return (
     <NavigationContainer>
       <StackNavigator />
+      {/* <AsideMenu /> */}
     </NavigationContainer>
   );
 }
