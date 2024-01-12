5198420b684ca9d84ae86f152d6076cabd9904b2
Hernan Mercado <hernandmf@gmail.com>
2024-01-12 17:53:26 -0500

    [add] menu toggle and drawer properties

--- navegationapp/src/router/AsideMenu.tsx
+++ navegationapp/src/router/AsideMenu.tsx
@@ -3,16 +3,20 @@ import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 import {StackNavigator} from './StackNavigator';
 import {SettingsScreen} from '../screens/SettingsScreen';
-import {PersonScreen} from '../screens/PersonScreen';
-import {Page1Screen} from '../screens/Page1Screen';
 import {Page2Screen} from '../screens/Page2Screen';
+import {useWindowDimensions} from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
 export const AsideMenu = () => {
+  const {width} = useWindowDimensions();
+
   return (
-    <Drawer.Navigator>
-      {/* <Drawer.Screen name="PersonScreen" component={PersonScreen} /> */}
+    <Drawer.Navigator
+      screenOptions={{
+        // drawerPosition: 'right',
+        drawerType: width >= 600 ? 'permanent' : 'front',
+      }}>
       <Drawer.Screen name="StackNavigator" component={StackNavigator} />
       <Drawer.Screen name="Page2Screen" component={Page2Screen} />
       <Drawer.Screen name="SettingsScreen" component={SettingsScreen} />
