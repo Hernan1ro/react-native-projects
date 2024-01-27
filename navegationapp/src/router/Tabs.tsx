7ba117f4ef82d88b38880cae85525ac3fb05fef2
Hernan Mercado <hernandmf@gmail.com>
2024-01-27 11:15:45 -0500

    [add] add icon to the tab naviation

--- navegationapp/src/router/Tabs.tsx
+++ navegationapp/src/router/Tabs.tsx
@@ -3,15 +3,50 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {Tab1screen} from '../screens/Tab1screen';
 import {Tab2screen} from '../screens/Tab2screen';
 import {Tab3screen} from '../screens/Tab3screen';
+import {StackNavigator} from './StackNavigator';
+import {Text} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export const Tabs = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Tab1Screen" component={Tab1screen} />
-      <Tab.Screen name="Tab2Screen" component={Tab2screen} />
-      <Tab.Screen name="Tab3Screen" component={Tab3screen} />
+    <Tab.Navigator
+      sceneContainerStyle={{
+        backgroundColor: 'white',
+      }}
+      screenOptions={{
+        tabBarActiveTintColor: '#efb810',
+        tabBarInactiveTintColor: 'black',
+        tabBarStyle: {elevation: 0},
+        tabBarLabelStyle: {
+          fontSize: 15,
+        },
+      }}>
+      {/* <Tab.Screen
+        name="Tab1Screen"
+        options={{
+          title: 'Tab1',
+          tabBarIcon: props => <Text style={{color: props.color}}>T1</Text>,
+        }}
+        component={Tab1screen}
+      /> */}
+      <Tab.Screen
+        name="Tab1Screen"
+        options={{
+          title: 'Tab1',
+        }}
+        component={Tab1screen}
+      />
+      <Tab.Screen
+        name="Tab2Screen"
+        options={{title: 'Tab2'}}
+        component={Tab2screen}
+      />
+      <Tab.Screen
+        name="StackNavigator"
+        options={{title: 'Stack'}}
+        component={StackNavigator}
+      />
     </Tab.Navigator>
   );
 };
--- navegationapp/src/screens/Tab1screen.tsx
+++ navegationapp/src/screens/Tab1screen.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
 
 export const Tab1screen = () => {
+  useEffect(() => {
+    console.log('Tab1Screen effect');
+  }, []);
+
   return (
     <View>
       <Text>Tab1Screen</Text>
